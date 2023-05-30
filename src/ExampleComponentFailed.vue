3464a865ccc2f187279611f5bf30105fe35b465c
yamanoku <0910yama@gmail.com>
2023-05-30 22:46:17 +0900

    init

---
+++ src/ExampleComponentFailed.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+export type ExampleComponentProps = {
+  examples: {
+    id: number;
+    name: string;
+  }[];
+};
+
+const props = defineProps<ExampleComponentProps>();
+</script>
+
+<template>
+  <ul v-if="props.examples.length > 0">
+    <!-- Error if comment inserted inside ul element -->
+    <li v-for="item in props.examples" :key="item.id">
+      {{ item.name }}
+    </li>
+  </ul>
+</template>
---
+++ src/ExampleComponentSafe.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+export type ExampleComponentProps = {
+  examples: {
+    id: number;
+    name: string;
+  }[];
+};
+
+const props = defineProps<ExampleComponentProps>();
+</script>
+
+<template>
+  <!-- No error if there are no comments inside -->
+  <ul v-if="props.examples.length > 0">
+    <li v-for="item in props.examples" :key="item.id">
+      {{ item.name }}
+    </li>
+  </ul>
+</template>
